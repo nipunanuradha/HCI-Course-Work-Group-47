8b4f3692ceb4d9810e8605adb2166fab07e25739
A.R.A.N Athukorala <135397733+nipunanuradha@users.noreply.github.com>
2025-04-27 00:53:32 +0530

    add init_.py
    add the developed version and author

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+3D Room and Furniture Modeling System
+
+A Python-based 3D modeling system for designing and visualizing room layouts with furniture.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Anuradha" 
